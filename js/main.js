e895dfc29c3162af35cddab77e08264ad057b624
ilokhov <ilokhov.dev@gmail.com>
2019-03-03 16:30:15 +0100

    use healthier

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 import { select } from "d3-selection";
 
 window.addEventListener("load", function() {
-  const width = 1040,
-    height = 520;
+  const width = 1040;
+  const height = 520;
 
-  const chart = select("#chart")
+  select("#chart")
     .append("svg")
     .attr("width", width)
     .attr("height", height);
