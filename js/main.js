f62673896cd516309a462a265da72419343d1d2e
ilokhov <ilokhov.dev@gmail.com>
2020-03-07 01:05:23 +0100

    update

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-import { select } from "d3-selection";
+import { select } from 'd3-selection';
 
-window.addEventListener("load", function() {
-  const width = 1040;
-  const height = 520;
+window.addEventListener('load', function() {
+    const width = 1040;
+    const height = 520;
 
-  select("#chart")
-    .append("svg")
-    .attr("width", width)
-    .attr("height", height);
+    select('#chart')
+        .append('svg')
+        .attr('width', width)
+        .attr('height', height);
 });
--- rollup.config.js
+++ rollup.config.js
@@ -1,28 +1,28 @@
-import resolve from "rollup-plugin-node-resolve";
-import buble from "rollup-plugin-buble";
-import { uglify } from "rollup-plugin-uglify";
-import serve from "rollup-plugin-serve";
-import livereload from "rollup-plugin-livereload";
+import resolve from 'rollup-plugin-node-resolve';
+import buble from 'rollup-plugin-buble';
+import { terser } from 'rollup-plugin-terser';
+import serve from 'rollup-plugin-serve';
+import livereload from 'rollup-plugin-livereload';
 
 const production = !process.env.ROLLUP_WATCH;
 
 export default {
-  input: "js/main.js",
-  output: {
-    name: "datavis",
-    file: "js/bundle.js",
-    format: "iife",
-    sourcemap: !production && true
-  },
-  plugins: [
-    resolve(),
-    !production &&
-      serve({
-        contentBase: "",
-        port: 4000
-      }),
-    !production && livereload(),
-    production && buble(),
-    production && uglify()
-  ]
+    input: 'js/main.js',
+    output: {
+        name: 'datavis',
+        file: 'js/bundle.js',
+        format: 'iife',
+        sourcemap: !production && true
+    },
+    plugins: [
+        resolve(),
+        !production &&
+            serve({
+                contentBase: '',
+                port: 4000
+            }),
+        !production && livereload(),
+        production && buble(),
+        production && terser()
+    ]
 };
